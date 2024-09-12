3cfa65ea2b1075ce21f2515e7e87d937e918726c
Ahmad Hassan Khan <codewithahk@gmail.com>
2024-09-12 23:07:15 +0500

    feat(controllers):added the declearation of the user authentication controller not yet added logic in controllers

---
+++ backend/dist/controllers/userAuthController.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.userLogoutController = exports.userLoginController = exports.userSignupController = void 0;
+const userSignupController = (req, res) => { };
+exports.userSignupController = userSignupController;
+const userLoginController = (req, res) => { };
+exports.userLoginController = userLoginController;
+const userLogoutController = (req, res) => { };
+exports.userLogoutController = userLogoutController;
