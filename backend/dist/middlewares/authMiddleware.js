91b7dee27f6ca39fe126c42de2cd938c0edf6199
Ahmad Hassan Khan <codewithahk@gmail.com>
2024-09-12 23:09:28 +0500

    feat(middlewares):not yet added logic in user authentication controller just declared

---
+++ backend/dist/middlewares/authMiddleware.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.authmiddleware = void 0;
+const authmiddleware = (req, res, next) => {
+};
+exports.authmiddleware = authmiddleware;
---
+++ backend/src/middlewares/authMiddleware.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+
+export const authmiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+
+
+
+
+    
+};
